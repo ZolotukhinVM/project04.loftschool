8678bcf203623da00f5028d50a40716f66919a60
Viktor Zolotukhin <zolotukhinvm@ya.ru>
2019-03-18 21:12:01 +0300

    #5 added buttons on detail page for admin

--- resources/views/user/product/detail.blade.php
+++ resources/views/user/product/detail.blade.php
@@ -41,5 +41,43 @@
             </table>
         </form>
     @endguest
+    @if(isset(Auth::user()->level) && Auth::user()->level == 1)
+        <div class="text-right">
+            <a href="{{ route('product.edit', ['id'=>$product->id]) }}"
+               class="btn btn-outline-secondary">
+                Update
+            </a>
+            @if(isset($product->order))
+                <a data-toggle="modal" data-target="#exampleModal" href="#"
+                   class="btn btn-outline-danger">
+                    Delete
+                </a>
+            @else
+                <a href="{{ route('product.destroy', ['id'=>$product->id]) }}"
+                   class="btn btn-outline-danger">
+                    Delete
+                </a>
+        </div>
+    @endif
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+         aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Product</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    Удаление невозможно! Товар находится в таблице заказов.
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endif
 
 @endsection
